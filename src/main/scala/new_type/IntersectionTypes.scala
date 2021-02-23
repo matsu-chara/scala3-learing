a8a1e1f4cf9b34d6eb84968179e94ebb3edae958
matsu-chara <matsuy00@gmail.com>
2021-02-23 14:19:23 +0900

    intersection type

--- src/main/scala/new_type/IntersectionTypes.scala
+++ src/main/scala/new_type/IntersectionTypes.scala
@@ -1,3 +1,23 @@
 package new_type
 
-class IntersectionTypes {}
+trait Resettable:
+  def reset(): Unit
+
+trait Growable[T]:
+  def add(t: T): Unit
+
+def f(x: Resettable & Growable[String]) =
+  x.reset()
+  x.add("first")
+
+trait A:
+  def children: List[A]
+
+trait B:
+  def children: List[B]
+
+trait C extends A,B:
+  def children: List[A&B] = List.empty[A&B]
+  
+val x: A & B = null
+val ys: List[A & B] = x.children
