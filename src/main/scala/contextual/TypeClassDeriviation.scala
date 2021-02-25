484b4309d5d73ae7908c9d4d529087320b2c48c3
matsu-chara <matsuy00@gmail.com>
2021-02-25 22:01:31 +0900

    type class deriviation using macro

--- src/main/scala/contextual/TypeClassDeriviation.scala
+++ src/main/scala/contextual/TypeClassDeriviation.scala
@@ -1,49 +1,49 @@
 import scala.deriving.Mirror
 import scala.compiletime.{erasedValue, summonInline}
 
-inline def summonAll[T <: Tuple]: List[Eq[_]] =
+inline def summonAll[T <: Tuple]: List[Eq1[_]] =
   inline erasedValue[T] match
     case _: EmptyTuple => Nil
-    case _: (t *: ts) => summonInline[Eq[t]] :: summonAll[ts]
+    case _: (t *: ts) => summonInline[Eq1[t]] :: summonAll[ts]
 
-trait Eq[T]:
-  def eqv(x: T, y: T): Boolean
+trait Eq1[T]:
+  def Eq1v(x: T, y: T): Boolean
 
-object Eq:
-  given Eq[Int] with
-    def eqv(x: Int, y: Int) = x == y
+object Eq1:
+  given Eq1[Int] with
+    def Eq1v(x: Int, y: Int) = x == y
 
-  def check(elem: Eq[_])(x: Any, y: Any): Boolean =
-    elem.asInstanceOf[Eq[Any]].eqv(x, y)
+  def check(elem: Eq1[_])(x: Any, y: Any): Boolean =
+    elem.asInstanceOf[Eq1[Any]].Eq1v(x, y)
 
   def iterator[T](p: T) = p.asInstanceOf[Product].productIterator
 
-  def eqSum[T](s: Mirror.SumOf[T], elems: => List[Eq[_]]): Eq[T] =
-    new Eq[T]:
-      def eqv(x: T, y: T): Boolean =
+  def Eq1Sum[T](s: Mirror.SumOf[T], elems: => List[Eq1[_]]): Eq1[T] =
+    new Eq1[T]:
+      def Eq1v(x: T, y: T): Boolean =
         val ordx = s.ordinal(x)
         (s.ordinal(y) == ordx) && check(elems(ordx))(x, y)
 
-  def eqProduct[T](p: Mirror.ProductOf[T], elems: => List[Eq[_]]): Eq[T] =
-    new Eq[T]:
-      def eqv(x: T, y: T): Boolean =
+  def Eq1Product[T](p: Mirror.ProductOf[T], elems: => List[Eq1[_]]): Eq1[T] =
+    new Eq1[T]:
+      def Eq1v(x: T, y: T): Boolean =
         iterator(x).zip(iterator(y)).zip(elems.iterator).forall {
           case ((x, y), elem) => check(elem)(x, y)
         }
 
-  inline given derived[T](using m: Mirror.Of[T]): Eq[T] =
+  inline given derived[T](using m: Mirror.Of[T]): Eq1[T] =
     lazy val elemInstances = summonAll[m.MirroredElemTypes]
     inline m match
-      case s: Mirror.SumOf[T]     => eqSum(s, elemInstances)
-      case p: Mirror.ProductOf[T] => eqProduct(p, elemInstances)
+      case s: Mirror.SumOf[T]     => Eq1Sum(s, elemInstances)
+      case p: Mirror.ProductOf[T] => Eq1Product(p, elemInstances)
 
-enum Opt[+T] derives Eq:
+enum Opt[+T] derives Eq1:
   case Sm(t: T)
   case Nn
 
 @main def test(): Unit =
   import Opt.*
-  val eqoi = summon[Eq[Opt[Int]]]
-  assert(eqoi.eqv(Sm(23), Sm(23)))
-  assert(!eqoi.eqv(Sm(23), Sm(13)))
-  assert(!eqoi.eqv(Sm(23), Nn))
+  val Eq1oi = summon[Eq1[Opt[Int]]]
+  assert(Eq1oi.Eq1v(Sm(23), Sm(23)))
+  assert(!Eq1oi.Eq1v(Sm(23), Sm(13)))
+  assert(!Eq1oi.Eq1v(Sm(23), Nn))
