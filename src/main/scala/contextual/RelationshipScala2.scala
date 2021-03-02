1830837615ecfc6a491a02c5ffa87f1e23cb20ab
matsu-chara <matsuy00@gmail.com>
2021-03-02 21:46:02 +0900

    relationship scala2

---
+++ src/main/scala/contextual/RelationshipScala2.scala
@@ -0,0 +1,12 @@
+package contextual
+
+// anonymous given instanceは以下の様な命名になる
+//given given_Ord_Int: Ord[Int]
+//given given_Ord_List_T[T](using ord: Ord[T]): Ord[List[T]]
+
+// implicit val scala 2
+// implicit val pos: Position = tree.sourcePos
+
+// scala 3
+// val pos: Position = tree.sourcePos
+// given Position = pos
