aa5647000158d73b666619396448423544917d29
matsu-chara <matsuy00@gmail.com>
2021-03-06 22:08:48 +0900

    macro2

--- src/main/scala/metaprogramming/MacroCheck.scala
+++ src/main/scala/metaprogramming/MacroCheck.scala
@@ -1,5 +1,5 @@
 package metaprogramming
 
-@main def macroCheck: Unit = 
+@main def macroCheck: Unit =
   println(Macros.assert(1 == 1))
-  println(Macros.assert(1 == 1+1-2+1+1))
+  println(Macros.assert(1 == (if (false) 1 else 2)))
--- src/main/scala/metaprogramming/Macros.scala
+++ src/main/scala/metaprogramming/Macros.scala
@@ -11,8 +11,9 @@ object Macros:
       throw AssertionError(s"failed assertion: ${${ showExpr(expr) }}")
   }
 
-  def showExpr(expr: Expr[Boolean])(using Quotes): Expr[String] =
-    '{ "<some source code>" } // Better implementation later in this document
+  def showExpr[T](expr: Expr[T])(using Quotes): Expr[String] =
+    val code: String = expr.show
+    Expr(code)
 
   def to[T: Type, R: Type](f: Expr[T] => Expr[R])(using Quotes): Expr[T => R] =
     '{ (x: T) => ${ f('x) } }
@@ -20,3 +21,47 @@ object Macros:
   def from[T: Type, R: Type](f: Expr[T => R])(using Quotes): Expr[T] => Expr[R] =
     (x: Expr[T]) => '{ $f($x) }
 
+  enum Exp:
+    case Num(n: Int)
+    case Plus(e1: Exp, e2: Exp)
+    case Var(x: String)
+    case Let(x: String, e: Exp, in: Exp)
+
+  import Exp.*
+  val exp = Plus(Plus(Num(2), Var("x")), Num(4))
+  val letExp = Let("x", Num(3), exp)
+
+
+  def compile(e: Exp, env: Map[String, Expr[Int]])(using Quotes): Expr[Int] =
+    e match
+      case Num(n) =>
+        Expr(n)
+      case Plus(e1, e2) =>
+        '{ 
+            ${ compile(e1, env) } + ${ compile(e2, env) }
+          }
+      case Var(x) =>
+        env(x)
+      case Let(x, e, body) =>
+        '{ 
+          val y: Int = ${ compile(e, env) }
+          ${ compile(body, env + (x -> 'y)) }
+         }
+
+
+  def map[T](arr: Expr[Array[T]], f: Expr[T] => Expr[Unit])
+            (using Type[T], Quotes): Expr[Unit] = '{
+    var i: Int = 0
+    while i < ($arr).length do
+      val element: T = ($arr)(i)
+      ${f('element)}
+      i += 1
+    }
+  
+  def sum(arr: Expr[Array[Int]])(using Quotes): Expr[Int] = '{
+    var sum = 0
+    ${ map(arr, x => '{sum += $x}) }
+    sum
+    }
+  
+  inline def sum_m(arr: Array[Int]): Int = ${sum('arr)}
