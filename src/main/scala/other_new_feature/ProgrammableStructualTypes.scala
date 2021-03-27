bbc0775e35a698c1b99104ba7bf33d98f1599ff9
matsu-chara <matsuy00@gmail.com>
2021-03-27 21:29:33 +0900

    ProgrammableStructualTypes

---
+++ src/main/scala/other_new_feature/ProgrammableStructualTypes.scala
@@ -0,0 +1,24 @@
+package other_new_feature
+
+object ProgrammableStructualTypes:
+  import scala.reflect.Selectable.reflectiveSelectable
+
+  type Closeable = { def close(): Unit }
+
+  class FileInputStream:
+    def close(): Unit = ()
+
+  class Channel:
+    def close(): Unit = ()
+
+  def autoClose(f: Closeable)(op: Closeable => Unit): Unit =
+    try op(f) finally f.close()
+
+  trait Vehicle extends reflect.Selectable:
+    val wheels: Int
+  
+  val i3 = new Vehicle: // i3: Vehicle { val range: Int }
+    val wheels = 4
+    val range = 240
+  
+  i3.range
