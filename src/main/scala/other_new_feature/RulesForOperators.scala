09e867da5abfea7e68f11912261dbe064194f96f
matsu-chara <matsuy00@gmail.com>
2021-03-28 22:28:07 +0900

    RulesForOperators

---
+++ src/main/scala/other_new_feature/RulesForOperators.scala
@@ -0,0 +1,36 @@
+package other_new_feature
+
+object RulesForOperators:
+  import scala.annotation.targetName
+
+  trait MultiSet[T]:
+    infix def union(other: MultiSet[T]): MultiSet[T]
+
+    def difference(other: MultiSet[T]): MultiSet[T]
+
+    @targetName("intersection")
+    def *(other: MultiSet[T]): MultiSet[T]
+
+  val s1, s2: MultiSet[Int] = ???
+
+  s1 union s2         // OK
+  s1 `union` s2       // also OK but unusual
+  s1.union(s2)        // also OK
+
+  s1.difference(s2)   // OK
+  s1 `difference` s2  // OK
+  s1 difference s2    // gives a deprecation warning
+
+  s1 * s2             // OK
+  s1 `*` s2           // also OK, but unusual
+  s1.*(s2)            // also OK, but unusual
+
+  val str = "hello"
+  ++ " world"
+  ++ "!"
+
+  def condition(x: Int, xs: List[Int]) =
+    x > 0
+    ||
+    xs.exists(_ > 0)
+    || xs.isEmpty
