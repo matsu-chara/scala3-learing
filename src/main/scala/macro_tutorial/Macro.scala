5ab8bc5efe41d2309c8192f7b16130908d02ab84
matsu-chara <matsuy00@gmail.com>
2021-05-03 21:33:18 +0900

    Macro part2

--- src/main/scala/macro_tutorial/Macro.scala
+++ src/main/scala/macro_tutorial/Macro.scala
@@ -8,3 +8,41 @@ object Macro:
   def inspectCode(x: Expr[Any])(using Quotes): Expr[Any] =
     println(x.show)
     x
+
+  inline def power(inline x: Double, inline n: Int) =
+    ${ powerCode('x, 'n) }
+
+  def pow(x: Double, n: Int): Double =
+    if n == 0 then 1 else x * pow(x, n - 1)
+
+  def powerCode(
+      x: Expr[Double],
+      n: Expr[Int]
+  )(using Quotes): Expr[Double] =
+    val value: Double = pow(x.valueOrError, n.valueOrError)
+    Expr(value)
+
+  def debugPowerCode(
+      x: Expr[Double],
+      n: Expr[Int]
+  )(using Quotes): Expr[Double] =
+    println(s"""powerCode
+       |  x := ${x.show}
+       |  n := ${n.show}""".stripMargin)
+    val code = powerCode(x, n)
+    println(s"  code := ${code.show}")
+    code
+
+  inline def sumNow(inline nums: Int*): Int =
+    ${ sumCode('nums) }
+
+  def sumCode(nums: Expr[Seq[Int]])(using Quotes): Expr[Int] =
+    nums match
+      case Varargs(numberExprs) => // numberExprs: Seq[Expr[Int]]
+        val numbers: Seq[Int] = numberExprs.map(_.valueOrError)
+        Expr(numbers.sum)
+      case _ =>
+        scala.sys.error(
+          "Expected explicit argument" +
+            "Notation `args: _*` is not supported."
+        )
