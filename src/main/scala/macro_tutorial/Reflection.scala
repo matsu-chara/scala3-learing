8f2f237f0caf3c25b0fd1bb207780e009fafe78a
matsu-chara <matsuy00@gmail.com>
2021-05-07 21:43:36 +0900

    reflection part1

---
+++ src/main/scala/macro_tutorial/Reflection.scala
@@ -0,0 +1,10 @@
+package macro_tutorial
+
+object Reflection:
+  import scala.quoted.* // Import `quotes`, `Quotes` and `Expr`
+
+  def f(x: Expr[Int])(using Quotes): Expr[Int] =
+    import quotes.reflect.*
+    val tree: Term = x.asTerm
+    val expr: Expr[Int] = tree.asExprOf[Int]
+    expr
