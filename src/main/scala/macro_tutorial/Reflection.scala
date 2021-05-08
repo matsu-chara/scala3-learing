223d91781a97ad13158ab1c40fa9be27f84a7dd3
matsu-chara <matsuy00@gmail.com>
2021-05-08 20:49:45 +0900

    reflection part2

--- src/main/scala/macro_tutorial/Reflection.scala
+++ src/main/scala/macro_tutorial/Reflection.scala
@@ -8,3 +8,14 @@ object Reflection:
     val tree: Term = x.asTerm
     val expr: Expr[Int] = tree.asExprOf[Int]
     expr
+
+  def debug(x: Expr[Int])(using Quotes): Expr[Int] =
+    import quotes.reflect.*
+    val tree: Tree = ???
+    println(tree.show(using Printer.TreeStructure))
+
+    tree match
+      case Ident(_)     => ???
+      case Select(_, _) => ???
+      case _ =>
+        throw new MatchError(tree.show(using Printer.TreeStructure))
